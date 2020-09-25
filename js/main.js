a08b57bd22f2d9ea3de1f75bd1da024037c84129
omoloye13 <69142994+omoloye13@users.noreply.github.com>
2020-09-25 16:34:45 +0100

    i updated the discount app

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 let Button=document.getElementById("btn")
 Button.addEventListener("click", total);
 function total(e) {
-e.preventDefault();
+    e.preventDefault();
 
 let ProductName=document.getElementById("proName").value;
 let itemQuantity=document.getElementById("unit").value;
@@ -13,22 +13,23 @@ discount20= 0.2* itemPrice;
 discount30= 0.3* itemPrice;
 discount40= 0.4* itemPrice;
 if (itemQuantity > 0 && itemQuantity <=3){
-     console.log(` You bought ${itemQuantity} ${ProductName} you will be paying #${itemQuantity*(itemPrice-discount10)} at 10% discount`) ;
+    document.getElementById("display").innerHTML = (` You bought ${itemQuantity} ${ProductName} you will be paying #${itemQuantity*(itemPrice-discount10)} at 10% discount`) ;
 }
 else if (itemQuantity > 3 && itemQuantity <=6) {
-    console.log(` You bought ${itemQuantity} ${ProductName}  you will be paying #${itemQuantity*(itemPrice-discount20)} at 20% discount`);
+    document.getElementById("display").innerHTML = (` You bought ${itemQuantity} ${ProductName}  you will be paying #${itemQuantity*(itemPrice-discount20)} at 20% discount`);
 }
 else if (itemQuantity > 6 && itemQuantity <=8) {
 
-    console.log(` You bought ${itemQuantity} ${ProductName} you will be paying #${itemQuantity*(itemPrice-discount30)} at 30% discount`);
+document.getElementById("display").innerHTML = (` You bought ${itemQuantity} ${ProductName} you will be paying #${itemQuantity*(itemPrice-discount30)} at 30% discount`);
 }
 else if (itemQuantity > 8 && itemQuantity <=10) {
-    console.log(`You bought ${itemQuantity} ${ProductName} you will be paying #${itemQuantity*(itemPrice-discount40)} at 40% discount` );
+document.getElementById("display").innerHTML = (`You bought ${itemQuantity} ${ProductName} you will be paying #${itemQuantity*(itemPrice-discount40)} at 40% discount` );
 }
 else{
-    console.log(`See you some other time we dont have ${ProductName} at the moment`);
+document.getElementById("display").innerHTML = (`See you some other time we dont have ${ProductName} at the moment`);
     
 }
+
 
 
 
